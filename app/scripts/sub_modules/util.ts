249b8238782929f2dd4eac16a29a15abc8fbb3d6
AOKI Yuuto <aoki@u-ne.co>
2019-01-16 14:15:55 +0900

    fix: #12 use Fetch API instead of XHR (#16)

--- app/scripts/sub_modules/util.ts
+++ app/scripts/sub_modules/util.ts
@@ -1,47 +1,27 @@
-export const httpGet = async (url: string, header: { [s: string]: string } = {}): Promise<XMLHttpRequest> => {
-  return new Promise<XMLHttpRequest>((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = (event) => {
-      if (xhr.readyState !== 4) {
-        return
-      }
-      return resolve(xhr) // OK
-    }
-    xhr.open('GET', url, true) // Async
-    Object.keys(header).forEach((key) => {
-      xhr.setRequestHeader(key, header[key])
-    })
-    xhr.withCredentials = true
-    xhr.send()
+interface Headers { [s: string]: string }
+interface Data { [s: string]: string | Blob }
+
+export const httpGet = async (url: string, headers: Headers = {}) => {
+  return fetch(url, {
+    headers,
+    credentials: 'include',
   })
 }
 
-export const httpPostForm = async (
-  url: string,
-  formData: { [s: string]: string | Blob },
-  header: { [s: string]: string } = {},
-): Promise<XMLHttpRequest> => {
-  return new Promise<XMLHttpRequest>((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
-    const form = new FormData()
-    Object.keys(formData).forEach((key) => {
-      form.append(key, formData[key])
-    })
-    xhr.onreadystatechange = (event) => {
-      if (xhr.readyState !== 4) {
-        return
-      }
-      return resolve(xhr) // OK
-    }
-    xhr.open('POST', url, true) // Async
-    Object.keys(header).forEach((key) => {
-      xhr.setRequestHeader(key, header[key])
-    })
-    xhr.withCredentials = true
-    xhr.send(form)
+export const httpPostForm = async (url: string, data: Data, headers: Headers = {}) => {
+  const formData = new FormData()
+  for (const key of Object.keys(data)) {
+    formData.append(key, data[key])
+  }
+
+  return fetch(url, {
+    method: 'POST',
+    headers,
+    credentials: 'include',
+    body: formData,
   })
 }
 
 export const getBase64Image = async (url: string): Promise<Blob> => {
-  return await fetch(url).then((r) => r.blob())
+  return (await fetch(url)).blob()
 }
