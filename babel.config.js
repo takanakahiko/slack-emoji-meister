e682a43791f9ff7775c9ab473a45309b1a5cc7ef
HANATANI Takuma <mail@potato4d.me>
2019-01-16 22:08:12 +0900

    Improve build environment (#18)
    * chore: Use babel.config.js intead of .babelrc
    
    * types: Always strict mode on TypeScript code
    
    * refactor: Use 2 spaces indent
    
    * chore: Use babel target node-current
    
    * chore: Use tsc dry-run on lint

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ],
+  ignore: [
+    'node_modules'
+  ]
+}
