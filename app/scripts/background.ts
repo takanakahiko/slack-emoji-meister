ea6cde4d8bb383d17b6ff54004f151190754b08f
AOKI Yuuto <aoki@u-ne.co>
2019-01-20 01:46:27 +0900

    new feature: save the slack workspace and choose a workspace in the context menu

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -3,25 +3,21 @@
 
 import { getSessionInfo, openLoginForm, uploadEmoji } from './sub_modules/slack'
 
-const onClickContextMenus = async (
-  info: chrome.contextMenus.OnClickData,
-  tab: chrome.tabs.Tab,
-) => {
-  if (!info.srcUrl) {
+const addEmojiToTeam = async (imageUrl: string | undefined, givenTeamName?: string) => {
+  if (!imageUrl) {
     return
   }
-  const imageUrl = info.srcUrl
-  const teamName = prompt(chrome.i18n.getMessage('promptTeamName'))
+  const teamName = givenTeamName || prompt(chrome.i18n.getMessage('promptTeamName'))
   if (!teamName) {
-    return
-  }
-  const emojiName = prompt(chrome.i18n.getMessage('promptEmojiName'))
-  if (!emojiName) {
     return
   }
   const sessionInfo = await getSessionInfo(teamName)
   if (!sessionInfo) {
     return openLoginForm(teamName)
+  }
+  const emojiName = prompt(chrome.i18n.getMessage('promptEmojiName'))
+  if (!emojiName) {
+    return
   }
   try {
     await uploadEmoji(teamName, emojiName, imageUrl, sessionInfo)
@@ -34,24 +30,59 @@ const onClickContextMenus = async (
       type: 'basic',
       title: chrome.i18n.getMessage('registrationSuccessTitle'),
       message: chrome.i18n.getMessage('registrationSuccessBody', [emojiName, teamName]),
-      iconUrl: info.srcUrl
+      iconUrl: imageUrl,
     })
+    return { teamName, emojiName, imageUrl, sessionInfo }
   } catch (e) {
     // For non-chrome browsers
     new Notification(chrome.i18n.getMessage('registrationSuccessTitle'), {
       body: chrome.i18n.getMessage('registrationSuccessBody', [emojiName, teamName]),
-      image: info.srcUrl
+      image: imageUrl,
     })
   }
 }
 
+const reloadContextMenu = () => {
+  chrome.storage.sync.get(['teams'], (storageGetResult) => {
+    chrome.contextMenus.removeAll()
+    const id = chrome.contextMenus.create({
+      title: chrome.i18n.getMessage('contextMenuTitle'),
+      contexts: ['image'],
+    })
+
+    const teams: string[] = storageGetResult.teams || []
+
+    for (const team of teams) {
+      chrome.contextMenus.create({
+        title: chrome.i18n.getMessage('contextMenuTitleForAddEmojiToExistingTeam', [team]),
+        contexts: ['image'],
+        parentId: id,
+        onclick(info) {
+          addEmojiToTeam(info.srcUrl, team)
+        },
+      })
+    }
+
+    chrome.contextMenus.create({
+      title: chrome.i18n.getMessage('contextMenuTitleForAddEmojiToNewTeam'),
+      contexts: ['image'],
+      parentId: id,
+      async onclick(info) {
+        const result = await addEmojiToTeam(info.srcUrl)
+        if (result && !teams.includes(result.teamName)) {
+          teams.push(result.teamName)
+          chrome.storage.sync.set({ teams }, () => {
+            reloadContextMenu()
+          })
+        }
+      },
+    })
+  })
+}
+
 chrome.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
-  chrome.contextMenus.create({
-    title: chrome.i18n.getMessage('contextMenuTitle'),
-    contexts: ['image'],
-    onclick: onClickContextMenus,
-  })
+  reloadContextMenu()
 })
 
 console.log('backgound')
