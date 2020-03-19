35bee7d7054c39ac14179b4b0a753b8433962ba8
takanakahiko <takanakahiko@gmail.com>
2020-03-20 04:22:31 +0900

    Support firefox (#193)
    * add i18n for error message
    
    * add pupup pages ui
    
    * update metadetas
    
    * update markup in popup.html
    
    * add addSlackEmoji message
    
    * add init function to background script
    
    * fix addEmojiToWorkspace
    
    * set script for popup
    
    * add case for firefox
    
    * add quotes rule to eslint
    
    * fix permission
    
    * fix placeholders format in i18n
    
    * lintfix
    
    * fix keys for getting local strage
    
    * fix typo
    
    * fix
    
    * fix typo
    
    * fix workflows

--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -1,1 +1,37 @@
 console.log('popup')
+
+const getInput = (id: string) => document.getElementById(id) as HTMLInputElement
+
+const setValues = ( imageUrl = '', workspaceName = '' ) => {
+    const imageUrlInput = getInput("imageUrl")
+    const workspaceNameInput = getInput("workspaceName")
+    imageUrlInput.value = imageUrl
+    workspaceNameInput.value = workspaceName
+
+    if(imageUrl == "") imageUrlInput.focus()
+    else if(workspaceName == "") workspaceNameInput.focus()
+    else getInput("emojiName").focus()
+}
+
+const onSubmit = (e:Event) => {
+    e.preventDefault()
+    const message = {
+        type: 'addSlackEmoji',
+        args: {
+            imageUrl : getInput("imageUrl").value,
+            workspaceName : getInput("workspaceName").value,
+            emojiName : getInput("emojiName").value
+        }
+    }
+    browser.runtime.sendMessage(message); 
+    close();
+}
+
+const init = () => {
+    setValues('', '')
+    browser.storage.onChanged.addListener(changes => setValues(changes.imageUrl.newValue, changes.workspaceName.newValue))
+    browser.storage.local.get().then(strage => setValues(strage.imageUrl, strage.workspaceName))
+    document.getElementById('form')?.addEventListener('submit', onSubmit)
+}
+
+window.onload = init
