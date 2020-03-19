f9b04abc0ce083f7dcf0b27964571cdb8f0cb635
takanakahiko <takanakahiko@gmail.com>
2020-03-20 04:02:41 +0900

    fix typo

--- app/scripts/sub_modules/contextmenu.ts
+++ app/scripts/sub_modules/contextmenu.ts
@@ -24,7 +24,7 @@ export const reloadContextMenu = () => {
           else if(isFirefox()){
             await browser.browserAction.openPopup() // This method is only available in firefox
             await browser.storage.local.set({
-              workspaceNam: '',
+              workspaceName: workspace,
               imageUrl: info.srcUrl
             })
           }
@@ -45,7 +45,7 @@ export const reloadContextMenu = () => {
         else if(isFirefox()){
           await browser.browserAction.openPopup() // This method is only available in firefox
           await browser.storage.local.set({
-            workspaceNam: '',
+            workspaceName: '',
             imageUrl: info.srcUrl
           })
         }
