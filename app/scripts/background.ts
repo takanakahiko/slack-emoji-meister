72efc0d72646adc220f7c0a94d7ff546e45a90a9
takanakahiko <takanakahiko@gmail.com>
2020-03-20 01:40:11 +0900

    add addSlackEmoji message

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -1,4 +1,5 @@
 import { reloadContextMenu } from './sub_modules/contextmenu'
+import { addEmojiToWorkspace } from './sub_modules/slack'
 
 browser.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
@@ -9,6 +10,12 @@ browser.runtime.onMessage.addListener((request)=>{
   if(request==='reloadContextMenu'){
     reloadContextMenu()
   }
+  if(request.type==='addSlackEmoji'){
+    const args = request.args
+    addEmojiToWorkspace(args.imageUrl, args.workspaceName, args.emojiName)
+  }
 })
+
+reloadContextMenu()
 
 console.log('backgound')
